5ea18971f2c075a16b2cbe60cec1ba3f367ddd78
IshanJawade <ishankjawade@gmail.com>
2026-04-16 16:06:18 -0700

    add failed python code

---
+++ basics/cmd_args.py
@@ -0,0 +1,11 @@
+# take command line arguments as a inputs
+
+import sys
+
+fileName = sys.argv[0] 
+first = sys.argv[1] 
+second = sys.argv[2]
+
+print("File Name: " + fileName)
+print("First Argument: " + first)
+print("Second Argument: " + second)
